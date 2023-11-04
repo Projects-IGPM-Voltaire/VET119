66ecfacfac4b4b05e2c161c9dbb70e3b0b7ec227
Sebastian Curtis T. Lavarias <sebastiancurtislavarias@gmail.com>
2023-11-04 23:23:54 +0800

    Gitignore reset

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header class="bg-primary container">
+      <q-toolbar>
+        <q-toolbar-title class="text-bold">Mediqueue </q-toolbar-title>
+        <q-space></q-space>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container class="container">
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "MainLayout",
+});
+</script>
+
+<script setup></script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page padding>
+    <HomeLandingBanner />
+    <HomeServicesBanner />
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import HomeLandingBanner from "components/HomeLandingBanner.vue";
+import HomeServicesBanner from "components/HomeServicesCard.vue";
+
+export default defineComponent({
+  name: "IndexPage",
+  components: { HomeServicesBanner, HomeLandingBanner },
+});
+</script>
