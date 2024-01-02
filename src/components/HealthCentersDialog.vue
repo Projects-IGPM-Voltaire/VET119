7a56f9626d22ddefb8b6321c4751fbc8d9fbe553
Sebastian Curtis T. Lavarias <sebastiancurtislavarias@gmail.com>
2024-01-02 18:00:23 +0800

    Health centers

--- src/components/HealthCentersDialog.vue
+++ src/components/HealthCentersDialog.vue
@@ -1,35 +1,76 @@
 <template>
   <q-dialog persistent v-model="modelValueLocal">
-    <q-card style="min-width: 600px">
+    <q-card style="min-width: 800px">
       <q-card-section class="row items-center q-pb-none">
         <div>
-          <div class="text-h6">Find other centers</div>
-          <div class="text-subtitle2">
-            Health centers at
-            {{ authUser.health_center_member.center.address.barangay.name }}
-          </div>
+          <div class="text-h6">Health centers using Mediqueue</div>
         </div>
         <q-space />
         <q-btn icon="close" flat round dense v-close-popup />
       </q-card-section>
       <q-card-section
         ><template v-for="(center, index) in healthCenters" :key="index">
-          <q-item>
-            <q-item-section top thumbnail class="q-ml-none">
-              <img
-                :src="toPublicImage(center.image.path)"
-                v-if="objetHasValue(center.image)"
-              />
-            </q-item-section>
+          <q-expansion-item>
+            <template v-slot:header>
+              <q-item-section avatar v-if="objetHasValue(center.image)">
+                <q-avatar size="xl" square>
+                  <img :src="toPublicImage(center.image.path)" />
+                </q-avatar>
+              </q-item-section>
 
-            <q-item-section>
-              <q-item-label>{{ center.name }}</q-item-label>
-              <q-item-label caption>{{ center.members.length }}</q-item-label>
-            </q-item-section>
+              <q-item-section class="text-capitalize">
+                <div class="text-h6 text-bold">
+                  {{ center.name }}
+                </div>
+                <div class="text-subtitle2">
+                  {{ toAddress(center.address) }}
+                </div>
+              </q-item-section>
+            </template>
 
-            <q-item-section side top> </q-item-section>
-          </q-item> </template
-      ></q-card-section>
+            <q-card>
+              <q-card-section>
+                <q-img width="100%" :src="toPublicImage(center.image.path)" />
+              </q-card-section>
+              <q-card-section>
+                <div class="text-subtitle2 q-mb-md">Doctors</div>
+                <div class="row">
+                  <template
+                    v-for="(member, index2) in center.members"
+                    :key="index2"
+                  >
+                    <div
+                      class="col-12 col-lg-4"
+                      v-if="member.position === 'doctor'"
+                    >
+                      <q-card class="my-card">
+                        <q-img
+                          :src="toPublicImage(member.user.image.path)"
+                          v-if="objetHasValue(member.user.image)"
+                        >
+                          <div
+                            class="absolute-bottom text-subtitle2 text-center"
+                          >
+                            {{ member.user.first_name }}
+                            {{ member.user.last_name }}
+                          </div>
+                        </q-img>
+                        <div
+                          class="absolute-bottom text-subtitle2 text-center"
+                          v-else
+                        >
+                          {{ member.user.first_name }}
+                          {{ member.user.last_name }}
+                        </div>
+                      </q-card>
+                    </div>
+                  </template>
+                </div>
+              </q-card-section>
+            </q-card>
+          </q-expansion-item>
+        </template></q-card-section
+      >
     </q-card>
   </q-dialog>
 </template>
@@ -51,6 +92,7 @@ import {
   convertTo12HourFormat,
   convertToDateReadable,
   debounce,
+  toAddress,
   toPublicImage,
 } from '../extras/misc';
 import { useHealthCenterStore } from 'stores/healthCenter';
@@ -79,10 +121,11 @@ watch(
 
 const getHealthCenters = async () => {
   const { code, data } = await healthCenterStore.list({
-    barangayCode:
-      authUser.value.health_center_member.center.address.barangay.code,
+    search: null,
+    barangayCode: null,
   });
   if (code === 200) {
+    console.log(data);
     healthCenters.value = data;
     return;
   }
